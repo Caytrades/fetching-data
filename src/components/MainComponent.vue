c834a77516563d610d9554bdc60e2e22d47dce4e
Cay <nanakwameopokuababio@gmail.com>
2024-06-11 10:27:30 +0000

    Updated

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,42 @@
+<script setup>
+import {ref} from 'vue'
+
+const data = ref(null)
+
+const fetchData = () => {
+  //Make a GET request using fetch
+  fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => {
+    //Check if the response status is OK
+    if(!response.ok){
+      throw new Error('HTTP Error Status: ${response.status}')
+    }
+
+    //Parse the JSON Response
+    return response.json()
+  }) .then((responseData) => {
+    data.value = responseData
+  }).catch((error) => {
+    console.error(`Error Fetching data: ${error}`)
+  })
+}
+
+</script>
+
+
 <template>
   <div>
-  
+    <h1>((Using Fetch))</h1>
+    <button @click="fetchData">Fetch Data</button>
+
+    <div>
+      <h2>Data From API</h2>
+      <pre>{{ data }}</pre>
+    </div>
   </div>
+  
 </template>
 
 
-<script>
-export default {
-  
-}
-</script>
 
 
 
